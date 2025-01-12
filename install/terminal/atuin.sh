0a8c8fbd96863969c30139df07b67be0fc6ddcce
michel gatti <michelxgatti@gmail.com>
2025-01-12 11:37:11 -0300

    Removing atuin as its not working properly

--- install/terminal/atuin.sh
+++
@@ -1,7 +0,0 @@
-yay -Sy --noconfirm atuin
-
-# if $(gum confirm "Press (Y) to register a new account into Atuin or (N) if want to log back in."); then
-#   atuin register
-# else
-#   atuin login
-# fi
